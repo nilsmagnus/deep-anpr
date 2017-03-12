3f01ff0f34d2fa9da5b8424131bcc34efeb853f3
Nils Larsgård <nilsmagnus@gmail.com>
2017-03-12 18:38:39 +0100

    synthesizer.py

--- synthesizer.py
+++ synthesizer.py
@@ -2,12 +2,16 @@ from PIL import Image
 from PIL import ImageFont, ImageDraw
 import numpy as np
 import random
+import os
 
+FONT_PATH = "./UKNumberPlate.ttf"
+FONT_SIZE = 22
 PLATE_SIZE = (20,94)
 IMAGE_SIZE = (512, 512)
 
 LETTERS = "ABCDEFGHIJKLMOPQRSTUVWXYZ"
-DIGITS= "0123456789"
+DIGITS = "0123456789"
+
 
 def random_plate_string(pattern ="AA DDDDD"):
     # default pattern is the norwegian standard plate
@@ -20,52 +24,55 @@ def random_plate_string(pattern ="AA DDDDD"):
     # e.g_ "AA-DDD-ZZ" could return "CQ-3256-ZZ"
     result = ""
     for c in pattern:
-        if (c == "A"):
+        if c == "A":
             result += random.choice(LETTERS)
-        elif (c == "D"):
+        elif c == "D":
             result += random.choice(DIGITS)
         else:
             result += c
     return result
 
+
 def random_coords(bounds, object):
-    maxX, maxY = bounds[0] - object[0], bounds[1]- object[1]
+    max_x, max_y = bounds[0] - object[0], bounds[1]- object[1]
+    ys = range(0, max_y)
+    xs = range(0, max_x)
+    return random.choice(xs),random.choice(ys)
 
-    ys = range(0, maxY)
-    xs = range(0, maxX)
-
-    return(random.choice(xs), random.choice(ys))
 
 def generate_plate(height, width, text, font, rotation=0):
     # Generates a white plate with the given text in black
-    plate = Image.new("L", (height, width), (255))
+    plate = Image.new("L", (width, height), (150))
     draw = ImageDraw.Draw(plate)
     draw.text((2, 2), text, (0), font=font)
-    if(rotation):
+    if rotation:
         plate = plate.rotate(rotation)
     return plate
 
 
-def add_plate_to_image(plate, image, xycoor):
-    size_x, size_y, _ = np.shape(plate)
-    coor_x, coor_y = xycoor
-    end_x, end_y = (coor_x + size_x), (coor_y + size_y)
-    image[coor_x:end_x, coor_y:end_y] =  plate
-    return image
+# 1 time: load font
+font = ImageFont.truetype(FONT_PATH, FONT_SIZE)
+
+# list all files in bgs
+bgs = os.listdir("bgs")
 
 
-def loadFont(font_path ="./UKNumberPlate.ttf"):
-    font = ImageFont.truetype(font_path, 22)
-    return font
+def generate(number=1):
+    plate_text = random_plate_string()
+    plate = generate_plate(PLATE_SIZE[0],PLATE_SIZE[1], plate_text,  font)
 
+    plate_position = random_coords(IMAGE_SIZE, plate.size)
 
-font = loadFont()
+    # open image, convert to grayscale and resize it to wanted size
+    background = Image.open("bgs/" + random.choice(bgs))
+    background.convert('L')
+    background = background.resize(IMAGE_SIZE, Image.BILINEAR)
 
-plate_text = random_plate_string()
-plate = generate_plate(PLATE_SIZE[1],PLATE_SIZE[0], plate_text,  font)
+    # paste plate onto image
+    background.paste(plate, plate_position)
 
-plate_position = random_coords(IMAGE_SIZE, plate.size)
-image = Image.new("L", IMAGE_SIZE, (0))
-image.paste(plate, plate_position)
+    return background, plate_position, plate_text
 
-image.show("image")
+im, pos, _ = generate()
+print "position is ", pos
+im.show("test")
